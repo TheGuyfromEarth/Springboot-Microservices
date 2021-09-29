ce50c086f66fc6f6d32c291299db12d505ebf6c2
TheGuyfromEarth <91563278+TheGuyfromEarth@users.noreply.github.com>
2021-09-29 16:14:56 +0530

    add : Eureka server microservice

---
+++ EurekaServer/src/test/java/com/payment/packages/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.packages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
